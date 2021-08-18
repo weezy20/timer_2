cdaeda0218e97726a76c57a59d875dc1156ef19d
Abhishek Shah <abhishekshah3@gmail.com>
2021-08-18 12:30:45 +0530

    Added a resume, and reuse functionality

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 const body = document.body;
 const timer = document.getElementById('timer');
 const clock = document.querySelector("#timer-container p");
-
 const btn = document.getElementById('start');
-btn.addEventListener("click", createTimer, { once: true });
+
+function main() {
+    clock.textContent = "00::00";
+    btn.addEventListener("click", createTimer, { once: true });
+}
 
 function createTimer() {
     const time = Number(timer.value);
     btn.textContent = "START";
-    btn.style.borderRadius = "0.7em";
+    btn.style.borderRadius = "2em";
+    btn.style.backgroundColor = "#522b29A0";
     // Weird JS can compare strings to numbers so this step works without the Number coercion as well
     let t = time < 10 ? '0' + time : time;
     clock.textContent = `${t}::00`;
@@ -16,23 +20,42 @@ function createTimer() {
 }
 
 function startTimer(time, evt) {
-    btn.innerText = "Pause"; // todo!()
+    btn.innerText = "PAUSE"; // todo!()
+    let isPaused = false;
+
     let seconds = 60 * time;
     let start_timer = setInterval(() => {
+        btn.addEventListener("click", () => {
+            isPaused = true;
+            btn.innerText = "RESUME";
+
+        });
         // Update text of clock
-        let s = seconds % 60;
-        let sec_hand = s >= 10 ? s : '0' + s;
-        let m = Math.floor(seconds / 60);
-        let min_hand = m >= 10 ? m : '0' + m;
-        let even = `${min_hand}:${sec_hand}`;
-        let odd = `${min_hand}:${sec_hand}`;
-        clock.innerText = s % 2 == 0 ? even : odd;
-        seconds--;
-        if (seconds == 0) {
-            clearInterval(start_timer);
-            clock.innerText = "TIME'S UP!";
+        if (!isPaused) {
+            let s = seconds % 60;
+            let sec_hand = s >= 10 ? s : '0' + s;
+            let m = Math.floor(seconds / 60);
+            let min_hand = m >= 10 ? m : '0' + m;
+            let even = `${min_hand}:${sec_hand}`;
+            let odd = `${min_hand}:${sec_hand}`;
+            clock.innerText = s % 2 == 0 ? even : odd;
+            seconds--;
+            if (seconds == 0) {
+                clearInterval(start_timer);
+                clock.innerText = "TIME'S UP!";
+                btn.style.backgroundColor = "none";
+                btn.borderRadius = "none";
+                btn.innerText = "Make Timer";
+                btn.addEventListener("click", main, { once: true });
+            }
+        } else {
+            btn.addEventListener("click", () => {
+                isPaused = false;
+                btn.innerText = "Pause";
+            })
         }
     }, 1000);
 
-    btn.removeEventListener("click", startTimer);
 }
+
+main();
